667f8976e9770383c6c029393c373cdc9d5a93df
krishnalasyatalluri <krishnalasyatalluri9@gmail.com>
2024-08-10 14:18:57 +0530

    added mongodb connection string

--- server/configureDB/db.js
+++ server/configureDB/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const configureDB = async (req, res) => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/task-management-app')
+        await mongoose.connect('mongodb+srv://krishnalasyatalluri9:Lasya0425@tasks.8tzjo.mongodb.net/?retryWrites=true&w=majority&appName=Tasks||process.env.MONGO_URI')
         console.log('db connected')
     } catch (e) {
         console.log('error connecting db')
